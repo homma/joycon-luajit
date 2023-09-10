b62eb131d9a9eb6da3726ce99b3a4606a2b712e7
Daisuke Homma <maada@Mac-mini.elecom>
2023-09-11 01:01:31 +0900

    committed on 2023/09/11 01:01.

--- read_button.lua
+++ read_button.lua
@@ -1,12 +1,9 @@
 local hidapi = require 'hidapi'
 local hidlib = require 'hidlib'
-
--- product_string Joy-Con (L)
-local vendor_id_l = 1406
-local product_id_l = 8198
+local joycon = require 'joycon'
 
 local main = function()
-  local dev = hidapi.hid_open(vendor_id_l, product_id_l, nil)
+  local dev = hidapi.hid_open(joycon.vendor_id_l, joycon.product_id_l, nil)
 
   if dev == nil then
     print 'cannot open device'
@@ -14,28 +11,7 @@ local main = function()
     os.exit(1)
   end
 
-  local output_1 = 0x1
-  local global_packet_number = 0
-
-  -- write buffer
-  local wlen = 64
-  local wbuf = hidlib.create_uchar_buffer(wlen)
-  wbuf:bzero()
-  wbuf.buf[0] = output_1
-  wbuf.buf[1] = global_packet_number
-  wbuf.buf[2] = 0x0
-  wbuf.buf[3] = 0x1
-  wbuf.buf[4] = 0x4
-  wbuf.buf[5] = 0x4
-  wbuf.buf[6] = 0x0
-  wbuf.buf[7] = 0x1
-  wbuf.buf[8] = 0x4
-  wbuf.buf[9] = 0x4
-  wbuf.buf[10] = 0x3
-  wbuf.buf[11] = 0x3f
-
-  -- write
-  hidapi.hid_write(dev, wbuf.buf, wbuf.length)
+  joycon.simple_hid_mode(dev)
 
   -- read buffer
   local rlen = 12
